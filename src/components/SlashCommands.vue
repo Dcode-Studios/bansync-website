57c4ce693339d34ae95c1509cedd1d805c94927f
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-04-20 11:53:31 -0700

    Create SlashCommands.vue

---
+++ src/components/SlashCommands.vue
@@ -0,0 +1,55 @@
+<template>
+<br>
+<div>
+  <div class="intro">
+    <h3>/ Slash Commands</h3>
+    <p>At the moment BanSync doesn't support much Slash Commands, but we hope to support more in the future!</p>
+  </div>
+  <img src="../assets/slash_commands.png">
+</div>
+</template>
+
+<script>
+export default {
+  name: 'BotLists'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  opacity: 1;
+  animation: fadeIn 1s ease;
+  margin: 4px;
+}
+@keyframes fadeIn{
+  0%{
+    opacity: 0;
+    }
+    100%{
+      opacity: 1;
+    }
+}
+div {
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  padding: 2px;
+}
+.intro {
+  color: rgb(170, 173, 176);
+  text-align: center;
+}
+.intro p {
+  color: #42b983;
+  text-align: center;
+}
+img {
+    width: 40%;
+  height: 40%;
+  min-width: 300px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
