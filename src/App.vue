9e1fefcc97f25f393439a84d74a53caf9b8eb0d2
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-04-20 11:53:06 -0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Intro/>
+  <BotLists/>
+  <SlashCommands/>
+</template>
+
+<script>
+import Intro from './components/Intro.vue'
+import BotLists from './components/BotLists.vue'
+import SlashCommands from './components/SlashCommands'
+
+export default {
+  name: 'App',
+  components: {
+    Intro,
+    BotLists,
+    SlashCommands
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+body {
+    background-color: black;
+}
+</style>
