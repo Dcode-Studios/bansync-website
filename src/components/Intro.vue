543a8fa726b445e010c2c987f533cc04254632ab
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-04-21 14:03:47 -0700

    Update Intro.vue

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -2,8 +2,8 @@
   <div class="hello">
     <img src="../assets/logo.png" class="logo">
     <div class="intro">
-      <h1>BanSync</h1>
-      <h4>Help keep out possible troublemakers from your server by knowing user's previous bans, or by linking your servers together to sync bans within them.</h4>
+      <h1 style="color: #42b983;">BanSync</h1>
+      <h4 id="topmsg">Help keep out possible troublemakers from your server by knowing user's previous bans, or by linking your servers together to sync bans within them.</h4>
     </div>
     <img class="bansync_helpmenu" src="../assets/bansync_help_menu.png">
     <a class="button" href="https://discord.com/oauth2/authorize?client_id=743234197484994610&permissions=347328&scope=applications.commands%20bot">Invite bot</a>
@@ -18,8 +18,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#topmsg {
+  max-width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+}
 .intro {
-  color: #42b983;
+  color: #ffffff;
   text-align: center;
 }
 .logo {
