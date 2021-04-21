614dfa34432c25481de138fd219f25efebecab82
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-04-21 12:57:03 -0700

    Update OtherResources.vue

--- src/components/OtherResources.vue
+++ src/components/OtherResources.vue
@@ -3,7 +3,7 @@
   <br>
   <h3>ℹ️ Other Resources</h3>
   <ol>
-    <li><a href="/privacy_policy.pdf">Privacy Policy</a></li>
+    <li><a class="button" href="/privacy_policy.pdf">Privacy Policy</a></li>
   </ol>
 </div>
 <br>
@@ -23,17 +23,6 @@ export default {
   max-width: 500px;
   margin-left: auto;
   margin-right: auto;
-}
-a {
-  color: rgb(255, 255, 255);
-  text-decoration:none;
-  border: 10px solid rgb(109, 109, 109);
-  background-color: rgb(109, 109, 109);
-  border-radius: 5px;
-  margin: 20px;
-}
-a :hover {
-  background-color: rgba(109, 109, 109, .5);
 }
 ol {
   list-style: none;
