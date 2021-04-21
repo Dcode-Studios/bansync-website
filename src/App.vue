94e34b08b42292ba4e268bc461a081a1a236f72d
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-04-21 09:37:29 -0700

    Update App.vue
    add button css

--- src/App.vue
+++ src/App.vue
@@ -38,4 +38,18 @@ export default {
 body {
     background-color: black;
 }
+.button {
+  color: rgb(255, 255, 255);
+  text-decoration:none;
+  border: 10px solid rgb(109, 109, 109);
+  background-color: rgb(109, 109, 109);
+  border-radius: 5px;
+  margin: 20px;
+  text-align: center;
+  margin-left: auto;
+  margin-right:auto;
+  display: block;
+  max-width: fit-content;
+  cursor: pointer;
+}
 </style>
