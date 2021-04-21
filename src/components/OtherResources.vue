f5d0b2461492dae0d6c6d8fecba00cd62c7a9ce8
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-04-20 23:34:05 -0700

    Create OtherResources.vue
    Add Privacy Policy link and GitHub link

---
+++ src/components/OtherResources.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="intro">
+  <br>
+  <h3>ℹ️ Other Resources</h3>
+  <ol>
+    <li><a href="/privacy_policy.pdf">Privacy Policy</a></li>
+    <br>
+    <li><a href="https://github.com/TechGeekGamer">Main Developer's GitHub (TechGeekGamer)</a></li>
+  </ol>
+</div>
+<br>
+</template>
+
+<script>
+export default {
+  name: 'OtherResources'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.intro {
+  color: #42b983;
+  text-align: center;
+  max-width: 500px;
+  margin-left: auto;
+  margin-right: auto;
+}
+a {
+  color: rgb(255, 255, 255);
+  text-decoration:none;
+  border: 10px solid rgb(109, 109, 109);
+  background-color: rgb(109, 109, 109);
+  border-radius: 5px;
+  margin: 20px;
+}
+a :hover {
+  background-color: rgba(109, 109, 109, .5);
+}
+ol {
+  list-style: none;
+  padding-left: 0px;
+}
+</style>
