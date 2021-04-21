548080135322aa82c71c33115a42ddb40893d2c1
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-04-21 14:03:40 -0700

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="intro">
-  <h3>About BanSync</h3>
+  <h3 style="color: #42b983;">About BanSync</h3>
   <p>
     BanSync is a Discord bot that can be used to:
     <ol>
@@ -8,9 +8,13 @@
       <li>🔁 Sync Bans between your servers</li>
     </ol>
     </p>
-    <h4>🔁 Linked Servers</h4>
+    <h4 style="color: #42b983;">🔁 Linked Servers</h4>
     <p>
       Linked Servers is a feature BanSync has to allow you to link up your servers to have bans synced within them. This means that if someone gets banned from one of the servers, they'll automatically be banned and blacklisted from all the linked servers.
+    </p>
+    <h4 style="color: #42b983;">📝 Global Ban Reason Database</h4>
+    <p>
+      BanSync has a feature that tracks bans you make, and shares information about it to other servers. This feature is <b>optional</b> and is <b><a style="color: red;">disabled</a> by default</b>. This can be <a style="color: green;">enabled</a>/<a style="color: red;">disabled</a> via the <code>&settings</code> command.
     </p>
 </div>
 </template>
@@ -24,10 +28,15 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .intro {
-  color: #42b983;
+  color: #ffffff;
   text-align: center;
   max-width: 500px;
   margin-left: auto;
   margin-right: auto;
+}
+ol {
+  list-style:circle;
+  padding-left: 0px;
+  text-align: left;
 }
 </style>
